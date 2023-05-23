b1371fd3ea468e3833c4dee9118586cfef904daa
Pabs <paul.mca.ca@gmail.com>
2023-05-23 01:28:45 +0000

    Refactor: Removed console log results and cleaned up the file for testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,25 @@
 const request = require('request');
-let breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
-  if (error !== null) {
-    console.error(`OH NO! There was an error!\nHere are the details:\n\n`, error);
-    return;
-  }
-  if (breed === undefined) {
-    console.log("Please give me a breed to work with.");
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      callback(`OH NO! There was an error!\nHere are the details:`);
+      callback(error);
+      return;
+    }
+    // if (breed === undefined) {
+    //   return "Please give me a breed to work with.";
+    // }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(`We don't have that breed on file. Maybe try one like "Chartreux" or "Siberian"`);
+      return;
+    }
+    if (response.statusCode === 200) {
+      callback(null, data[0].description);
+      return;
+    }
+  });
+};
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log(`We don't have that breed on file. Maybe try one like "Chartreux" or "Siberian"`);
-    return;
-  }
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
